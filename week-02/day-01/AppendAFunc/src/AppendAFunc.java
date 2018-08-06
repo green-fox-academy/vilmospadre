9ba6dbac1d383b5515f27224d3b917a4c7ab121d
vilmospadre <vilmospadre@gmail.com>
2018-08-06 15:51:56 +0200

    finished task AppendAFunc

---
+++ week-02/day-01/AppendAFunc/src/AppendAFunc.java
@@ -0,0 +1,13 @@
+public class AppendAFunc {
+    public static void main(String[] args) {
+
+        String typo = "Chinchill";
+        System.out.println(appendAFunc(typo));
+
+    }
+
+    public static String appendAFunc(String typo) {
+        String appendAFunc = typo + "a";
+        return appendAFunc;
+    }
+}
