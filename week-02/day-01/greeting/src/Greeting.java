3b2f01e14f6b475915553db8c20566f1abb00071
vilmospadre <vilmospadre@gmail.com>
2018-08-06 15:29:01 +0200

    finished task greetin

---
+++ week-02/day-01/greeting/src/Greeting.java
@@ -0,0 +1,12 @@
+public class Greeting {
+    public static void main(String[] args) {
+
+        String a = "Greenfox";
+        System.out.println(greeting(a));
+    }
+
+    public static String greeting(String a1) {
+        return "Greetings, GreenFoxlings! \n" + a1; //sortörés /n  mindig aposztróf között!
+
+    }
+}
