8991036971f1f14905951ffb2f8f56304619b62d
vilmospadre <vilmospadre@gmail.com>
2018-08-15 13:19:57 +0200

    added PostIt

---
+++ week-03/day-03/PostIt/src/PostIt.java
@@ -0,0 +1,15 @@
+public class PostIt {
+
+    String backGroundColor;
+    String text;
+    String textColor;
+
+    public PostIt(String backGroundColor, String text, String textColor) {
+        this.backGroundColor = backGroundColor;
+        this.text = text;
+        this.textColor = textColor;
+
+
+    }
+}
+
---
+++ week-03/day-03/PostIt/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        PostIt one = new PostIt("Orange", "Idea 1", "Blue");
+        PostIt two = new PostIt("Pink", "Awesome!", "Black");
+        PostIt three = new PostIt("Yellow", "Superb!", "Green");
+
+    }
+}
