dbca05a8e6f03d0b4e14f3ed1d42a81adb65f746
vilmospadre <vilmospadre@gmail.com>
2018-08-30 13:42:15 +0200

    repeated countAs as quick practice

---
+++ gyak/countAS/src/countAs.java
@@ -0,0 +1,23 @@
+public class countAs {
+    public static void main(String[] args) {
+        // Create a function that takes a string parameter,
+        // counts the occurrences of the letter "a", and returns it as a number.
+
+        System.out.println(countAs("Apple"));
+        System.out.println(countAs("Avocado"));
+        System.out.println(countAs("Blueberry"));
+        // example: on the input "Apple" the function should return 1 - print this result
+        // example: on the input "Avocado" the function should return 2 - print this result
+        // example: on the input "Blueberry" the function should return 0 - print this result
+    }
+
+    private static int countAs(String text) {
+        int count = 0;
+        for (char character : text.toLowerCase().toCharArray()) {
+            if (character == 'a') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
